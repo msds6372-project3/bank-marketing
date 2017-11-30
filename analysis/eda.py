2cd736d276d63ffd5ee65e1a5745253d83161005
Jostein Straume <joebarrystraume@gmail.com>
2017-11-30 12:13:26 -0800

    Load in the data

--- analysis/eda.py
+++ analysis/eda.py
@@ -1,2 +1,11 @@
-import pandas as pandas
-import numpy as numpy
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as mt
+import scipy as sp
+
+# Source: https://www.datacamp.com/community/tutorials/exploratory-data-analysis-python
+# Load in the data
+filepath = "/Users/Jostein/Grad School/SMU/6372/project3/bank-marketing/data/bank-full.csv"
+bank = pd.read_csv(filepath, header='infer', delimiter=';')
+bank.describe()
+print(bank)
